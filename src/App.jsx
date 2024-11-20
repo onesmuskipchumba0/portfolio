cacbdb5e8f0f4165b9b079363957a689ae8a5bad
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-11-20 14:49:04 +0300

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      {/* Other content will go here */}
+    </div>
   )
 }
 
